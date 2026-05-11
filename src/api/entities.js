4f742882bf35ee4709670153cb92b20c58c5371f
WFMs-dev <tanawin.sb@gmail.com>
2026-05-11 19:07:23 +0700

    Add src/api/entities.js

---
+++ src/api/entities.js
@@ -0,0 +1,13 @@
+import { base44 } from './base44Client';
+
+export const Employee = base44.entities.Employee;
+export const Shift = base44.entities.Shift;
+export const Schedule = base44.entities.Schedule;
+export const LeaveType = base44.entities.LeaveType;
+export const LeaveRequest = base44.entities.LeaveRequest;
+export const ShiftSwapRequest = base44.entities.ShiftSwapRequest;
+export const AttendanceLog = base44.entities.AttendanceLog;
+export const Holiday = base44.entities.Holiday;
+export const Notification = base44.entities.Notification;
+export const ExportJob = base44.entities.ExportJob;
+export const AuditLog = base44.entities.AuditLog;
